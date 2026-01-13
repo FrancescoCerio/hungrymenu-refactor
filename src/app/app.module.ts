9ec1aa9624c56f55dac87bc3b8534a618702448c
Francesco Cerio <francescocerio98@gmail.com>
2026-01-13 16:19:46 +0100

    Update to Angular 20 + fix code bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { map, call, menu, menuOutline, menuSharp, close, closeOutline, closeSharp } from 'ionicons/icons';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SectionModule } from './section/section.module';
+
+// Register icons globally for Ionic 8 + Vite
+addIcons({ map, call, menu, menuOutline, menuSharp, close, closeOutline, closeSharp });
 
 
 @NgModule({
