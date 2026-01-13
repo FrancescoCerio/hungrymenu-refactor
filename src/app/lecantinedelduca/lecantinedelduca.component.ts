ed03db317f4bb701317a43d276d15d892ad2cb6e
francesco_cerio <francescocerio98@gmail.com>
2026-01-13 15:33:57 +0100

    Upgrade Angular 19 (#19)

--- src/app/lecantinedelduca/lecantinedelduca.component.ts
+++ src/app/lecantinedelduca/lecantinedelduca.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-lecantinedelduca',
-  templateUrl: './lecantinedelduca.component.html',
-  styleUrls: ['./lecantinedelduca.component.scss'],
+    selector: 'app-lecantinedelduca',
+    templateUrl: './lecantinedelduca.component.html',
+    styleUrls: ['./lecantinedelduca.component.scss'],
+    standalone: false
 })
 export class LecantinedelducaComponent {
 
