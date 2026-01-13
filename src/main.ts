3b7c1b3eaba0fa3f84a92c8a442bc5fe6b3946fb
francesco_cerio <francescocerio98@gmail.com>
2026-01-13 15:25:36 +0100

    Upgrade Angular v18 (#18)
    * Upgrade Angular v18
    
    * Upgrade Angular v18 - add missing package-lock.json
    
    * Add Vercel config
    
    * Add Vercel Analytics and Speed Insights

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -7,6 +9,10 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+
+// Initialize Vercel Analytics and Speed Insights
+inject();
+injectSpeedInsights();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
