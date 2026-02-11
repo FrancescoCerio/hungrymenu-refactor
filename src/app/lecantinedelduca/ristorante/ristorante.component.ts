2455f3b0c6b60b022c0a0f2aead6285fd50053cc
Francesco Cerio <francescocerio98@gmail.com>
2026-02-11 16:50:20 +0100

    Add remote menu data fetching and skeleton on loading

--- src/app/lecantinedelduca/ristorante/ristorante.component.ts
+++ src/app/lecantinedelduca/ristorante/ristorante.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { restaurantMenu } from 'src/menus/lecantinedelduca/ristorante';
+import { Component, OnInit } from '@angular/core';
 import { Section } from 'src/models/section';
+import { SupabaseService } from 'src/app/services/supabase.service';
 
 @Component({
     selector: 'app-ristorante',
@@ -8,10 +8,24 @@ import { Section } from 'src/models/section';
     styleUrls: ['./ristorante.component.scss'],
     standalone: false
 })
-export class RistoranteComponent {
-  restaurantMenu: Section[] = restaurantMenu;
-  constructor() { }
+export class RistoranteComponent implements OnInit {
+  restaurantMenu: Section[] = [];
+  loading = true;
 
+  constructor(private supabaseService: SupabaseService) { }
+
+  ngOnInit(): void {
+    this.supabaseService.getMenu('lecantinedelduca', 'ristorante').subscribe({
+      next: (sections) => {
+        this.restaurantMenu = sections;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Failed to load menu:', err);
+        this.loading = false;
+      }
+    });
+  }
 
   scrollToElem(tag: string){
     const element: HTMLElement = document.getElementById(tag);
